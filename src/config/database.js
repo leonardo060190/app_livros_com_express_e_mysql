e9a035297052ba891152248982b85c147ba24ebe
leonardo060190 <leonardo-morruga@hotmail.com>
2023-07-14 22:53:49 -0300

    ok

--- src/config/database.js
+++ src/config/database.js
@@ -2,8 +2,8 @@ module.exports = {
     dialect: 'mysql', //tipo de linguagem 
     host: 'localhost',//local a onde está o banco de dados
     username: 'root',
-    port: '3306',// porta do banco de dados
-    password: 'root',//senha do banco de dados
+    port: '',// porta do banco de dados
+    password: '',//senha do banco de dados
     database: 'app_Livros',//nome do banco de dados que vai utilizar no projeto
     define: { 
         timestamps: false,//data de crição e data de alteração
